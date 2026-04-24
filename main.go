cf61907b6ab1b32165bf10b6a55a45070c474163
ayushmanmishra18 <ayushmanmishraji1@gmail.com>
2026-04-24 14:15:38 +0530

    new push

--- main.go
+++ main.go
@@ -6,6 +6,20 @@ import (
 	"encoding/json"
  ) 
 
+ type Address struct {
+	City string
+	State string
+	Country string
+	PinCode string
+ }
+ type user struct{
+	Name string
+	Age json.Number
+	Contact string
+	Company string
+	Address string
+ }
+
 func main() {
 	println("Hello, World!")
 }
